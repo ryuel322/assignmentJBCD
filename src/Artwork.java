f6a20c1fcff3fc60f8885200cb09935eeb637c93
Dayana <242029@astanait.edu.kz>
2025-01-21 00:05:24 +0500

    Update Artwork.java

--- src/Artwork.java
+++ src/Artwork.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+class Artwork extends Object {
+    private String title;
+    private Artist artist;
+    private Gallery gallery;
 
-public class Artwork {
+    public Artwork(String title, Artist artist, Gallery gallery) {
+        this.title = title;
+        this.artist = artist;
+        this.gallery = gallery;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public Gallery getGallery() {
+        return gallery;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Artwork: " + title + ", Artist: " + artist.getName() + ", Gallery: " + gallery.getName();
+    }
 }
